0263edc68a5a48d37b7d28710ad638cac76bbaba
longruchun <1430468717@qq.com>
2018-09-17 11:57:15 +0800

    1

--- src/com/lrc20180916_1/Test1.java
+++ src/com/lrc20180916_1/Test1.java
@@ -14,12 +14,12 @@ public class Test1 {
     	Scanner input=new Scanner(System.in);
 		System.out.println("请输入被除数");
 		int num1=input.nextInt();
-		 logger.debug("除数");
+		 logger.debug("被除数除数"+num1);
 		 System.out.println("请输入除数");
 			int num2=input.nextInt();
-			 logger.debug("除数");
+			 logger.debug("除数"+num2);
 			System.out.println(num1+"+"+num2+"="+(num1+num2)); 
-			 logger.debug("结果");
+			 logger.debug("结果"+(num1+num2));
 			 
     	
     	
